aa39ff95cb76ab0a9c736bca3c99322ce5e6c56d
Thomas Green <TomG@AccelHomeBuyers.com>
2014-08-17 13:15:54 -0600

    cap additions 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :deploy_to, '/home/reversg3/public_html/staging-smallcannabis'
 
 # Default value for :scm is :git
 set :scm, :git
-set :branch, 'master'
+set :branch, '77073032_capistrano_master'
 
 # Default value for :format is :pretty
 set :format, :pretty
