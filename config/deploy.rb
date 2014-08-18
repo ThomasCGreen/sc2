a3f296747803a4e0b689cae33ee6c2713d5ac3af
Thomas Green <TomG@AccelHomeBuyers.com>
2014-08-17 20:17:59 -0600

    more

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      run "touch #{current_path}/tmp/restart.txt"
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
