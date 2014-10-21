173792f5e98a93a556eebc692e2b33c3d2a47c21
Thomas Green <TomG@AccelHomeBuyers.com>
2014-10-20 20:26:25 -0600

    point capistrano to sc2.git

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,7 @@ set :tmp_dir, '/home/reversg3/tmp/smallcannabis'
 
 # Default value for :scm is :git
 set :scm, :git
+set :branch, ENV['BRANCH'] || 'master'
 
 # Default value for :format is :pretty
 set :format, :pretty
